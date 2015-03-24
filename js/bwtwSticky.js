ab199928fee21dca465b954ffcf891cec5f2c799
John Thomas <thomas07@vt.edu>
2015-03-24 09:41:33 -0700

    step 2 of fixing display issues on github.

--- js/bwtwSticky.js
+++ js/bwtwSticky.js
@@ -27,67 +27,67 @@
 //////////////////////////////////////////////////
 
 var bwtwSticky = (function() {
-var object = {};
+  var object = {};
 
-function init(id, classNames, offset) {
-	// console.log("INIT HIT: " + id + " " + offset);
-	object.html = document.getElementById(id);
-	object.offset = offset || getYOffset(object.html);
+  function init(id, classNames, offset) {
+  	// console.log("INIT HIT: " + id + " " + offset);
+  	object.html = document.getElementById(id);
+  	object.offset = offset || getYOffset(object.html);
 
-	object.originalClasses = object.html.className;
-	object.allClasses = object.originalClasses + " " + classNames;
-	object.stuck = object.stuck || false;
+  	object.originalClasses = object.html.className;
+  	object.allClasses = object.originalClasses + " " + classNames;
+  	object.stuck = object.stuck || false;
 
-	// Add scroll and resize event listener
-  window.addEventListener('scroll', bwtwSticky.stickyNavScroll, false);
-  window.addEventListener('resize', bwtwSticky.resize, false);
+  	// Add scroll and resize event listener
+    window.addEventListener('scroll', bwtwSticky.stickyNavScroll, false);
+    window.addEventListener('resize', bwtwSticky.resize, false);
 
-}
-
-function stickyNavScroll() {
-
-  // console.log("Scrolled: ");
-  if (window.scrollY >= object.offset && object.stuck == false) {
-		object.html.className = object.allClasses;
-		object.stuck = true;
   }
 
-  if (window.scrollY < object.offset && object.stuck == true) {
-    object.html.className = object.originalClasses;
-    object.stuck = false;
+  function stickyNavScroll() {
+
+    // console.log("Scrolled: ");
+    if (window.scrollY >= object.offset && object.stuck == false) {
+  		object.html.className = object.allClasses;
+  		object.stuck = true;
+    }
+
+    if (window.scrollY < object.offset && object.stuck == true) {
+      object.html.className = object.originalClasses;
+      object.stuck = false;
+    }
+
+
+  }
+
+  function getYOffset(element) {
+
+    var yPosition = 0;
+    while(element) {
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return yPosition;
+  }
+
+  function resize() {
+  	// console.log("resize hit");
+  	object.offset = getYOffset(object.html);
   }
 
 
-}
-
-function getYOffset(element) {
-
-  var yPosition = 0;
-  while(element) {
-      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-      element = element.offsetParent;
+  //// PUBLIC METHODS
+  return {
+    init: function(id, classNames, offset) {
+      return init(id, classNames,  offset);
+    },
+    stickyNavScroll: function() {
+      return stickyNavScroll();
+    },
+    resize: function() {
+    	return resize();
+    }
   }
-
-  return yPosition;
-}
-
-function resize() {
-	// console.log("resize hit");
-	object.offset = getYOffset(object.html);
-}
-
-
-//// PUBLIC METHODS
-return {
-  init: function(id, classNames, offset) {
-    return init(id, classNames,  offset);
-  },
-  stickyNavScroll: function() {
-    return stickyNavScroll();
-  },
-  resize: function() {
-  	return resize();
-  }
-}
 
 })();
